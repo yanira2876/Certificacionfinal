2d34d99440f9eb2306342369dac971fbabc3d438
yanira2876 <yanira.cayo.cuevas@alumnos.sip.cl>
2024-11-27 10:30:23 -0300

    first commit

---
+++ js.js
@@ -0,0 +1,12 @@
+function contact(){
+    alert ("Inicio de Sesión exitoso")
+}
+
+let numero = 0;
+
+function comprar(){
+    numero +=1;
+
+
+    document.getElementById("numero").innerText = numero;
+}
